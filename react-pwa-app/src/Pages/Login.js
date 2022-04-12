ae87133b4c21597645930da2e0a9dc036123ca6a
Bachar Hafez <expc7c19@gmail.com>
2022-04-13 02:58:08 +0300

    added react-router & images folder

---
+++ react-pwa-app/src/Pages/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Login() {
+  return (
+    <>
+      <img src="" alt="" className="logo" />
+      <h1 className="title">Login</h1>
+      <p className="label">
+        Enter your login details to sign in to your account
+      </p>
+      <input type="text" className="email" placeholder="Email" />
+      <input type="text" className="password" />
+      <button className="login-button"></button>
+      <a href="" className="social-links"></a>
+      <a href="" className="social-links"></a>
+      <a href="" className="social-links"></a>
+      <p className="label"></p>
+      <button className="guest-button"></button>
+    </>
+  );
+}
+
+export default Login;
