d161dd91fb6be86158da0ef190d38a2be8cf1762
Bachar Hafez <expc7c19@gmail.com>
2022-06-13 00:45:56 +0300

    implementing chat 1 added button to call police added userType store to store user type more authentication

--- react-pwa-app/src/App.js
+++ react-pwa-app/src/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import AdminApp from "./Admin-app/AdminApp";
 import AgentApp from "./Agent-app/AgentApp";
 import AuthoritiesApp from "./Authorities-app/AuthoritiesApp";
 import Login from "./Pages/Login";
 import Page404 from "./Pages/Page404";
+// User App pages
 import Profile from "./User-app/Pages/Profile";
 import Register from "./User-app/Pages/Register";
 import Report from "./User-app/Pages/Report";
 import SmsVerify from "./User-app/Pages/SmsVerify";
 import UserApp from "./User-app/UserApp";
+// Authorities App pages
+import NewReports from "./Authorities-app/Pages/NewReports";
+
+import Pusher from "pusher-js";
+import config from "../src/API/config.json";
 
 function App() {
+  useEffect(() => {
+    // const pusher = new Pusher("f06fc2e0e3a78a7ca79b", {
+    //   cluster: "eu",
+    //   encrypted: true,
+    // });
+    // const channel = pusher.subscribe("chat.2");
+    // channel.bind("message", (data) => {
+    //   console.log(data);
+    // });
+    // console.log(pusher);
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -20,14 +38,14 @@ function App() {
           <Route path="user" element={<UserApp />}>
             <Route path="register" element={<Register></Register>}></Route>
             <Route path="sms-verify" element={<SmsVerify></SmsVerify>}></Route>
-            <Route path="report" element={<Report></Report>}></Route>
+            <Route index element={<Report></Report>}></Route>
             <Route path=":userid" element={<Profile></Profile>}></Route>
 
             <Route path="*" element={<Page404></Page404>}></Route>
           </Route>
 
           <Route path="agent" element={<AgentApp />}></Route>
-          <Route path="authority" element={<AuthoritiesApp />}></Route>
+          <Route path="authority" element={<AuthoritiesApp />} />
           <Route path="admin" element={<AdminApp />}></Route>
           <Route path="*" element={<Page404></Page404>}></Route>
         </Routes>
---
+++ react-pwa-app/src/Authorities-app/Components/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Nav, Tab, Row, Col } from "react-bootstrap";
+
+const NavBar = () => {
+  return <></>;
+};
+
+export default NavBar;
