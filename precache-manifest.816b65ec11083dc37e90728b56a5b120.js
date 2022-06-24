ad0c98ba46752f5c1978b45bd19fa550713186d7
Bachar Hafez <expc7c19@gmail.com>
2022-06-24 23:15:48 +0300

    trying to Deploy React app to GitHub Pages

---
+++ precache-manifest.816b65ec11083dc37e90728b56a5b120.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e34ca5e70c889dcf17c",
+    "url": "/emergency-frontend/static/css/main.76101d35.chunk.css"
+  },
+  {
+    "revision": "5e34ca5e70c889dcf17c",
+    "url": "/emergency-frontend/static/js/main.5e34ca5e.chunk.js"
+  },
+  {
+    "revision": "c89d29946fbedf7225b6",
+    "url": "/emergency-frontend/static/css/1.29696771.chunk.css"
+  },
+  {
+    "revision": "c89d29946fbedf7225b6",
+    "url": "/emergency-frontend/static/js/1.c89d2994.chunk.js"
+  },
+  {
+    "revision": "ca353803bdc1c7af7f91",
+    "url": "/emergency-frontend/static/js/runtime~main.ca353803.js"
+  },
+  {
+    "revision": "b57a58905370284f1499d1a35f5d2c96",
+    "url": "/emergency-frontend/static/media/bootstrap-icons.b57a5890.woff2"
+  },
+  {
+    "revision": "a6a4862c8f0a585c6c1f6d5aa07b761c",
+    "url": "/emergency-frontend/static/media/bootstrap-icons.a6a4862c.woff"
+  },
+  {
+    "revision": "928d5919f204c045438bc0abbef808c0",
+    "url": "/emergency-frontend/index.html"
+  }
+];
